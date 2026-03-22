6c8e34545d928f4ecd0b1c7c2b936ff6ad227c7b
MomDontG0 <60045487+JosestebanPorrasV@users.noreply.github.com>
2026-03-22 12:40:01 -0600

    Agregar prueba uno de python y C
    Prueban 1 en  C y python

--- Porras_Josesteban_PruebaCorta1/prueba1.c
+++ Porras_Josesteban_PruebaCorta1/prueba1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+
+// Nombre: Josesteban Porras Villarreal
+
+int main(){
+
+    char nombreCompleto[]= "Josesteban Porras Villarreal";
+    char identiciación[20];
+
+    printf("Ingrese su numero de identiciacion");
+    scanf("%s",identiciación);
+
+    printf("Primer Apellido: Porras\n");
+    printf("Segundo Apellido: Villarreal\n");
+     printf("Nombre: Josesteban\n");
+
+     return 0;
+}
 
--- Porras_Josesteban_PruebaCorta1/prueba1.py
+++ Porras_Josesteban_PruebaCorta1/prueba1.py
@@ -1,1 +1,17 @@
 #Nombre: Josesteban Porras Villarreal
+
+nombreCompleto ="Josesteban Porras Villarreal"
+id = input("Ingrese su número de identificación: ")
+
+
+partes = nombreCompleto.split()
+nombre = partes[0]
+apellido1 = partes[1]
+apellido2 = partes[2]
+
+print("Primer Apellido",apellido1)
+print("Segundo Apellido",apellido2)
+print("Nombre",nombre)
+print("Identificacion",id)
+
+# Nombre: Josesteban Porras Villarreal
